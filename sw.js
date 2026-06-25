d27f7c485c71303ebb3c1d6e2ea1b780b5656829
alaamesmar2-svg <alaa.mesmar2@gmail.com>
2026-06-26 00:43:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,14 @@
-const CACHE_NAME = 'clinic-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png'
-];
+const CACHE_NAME = 'clinic-cms-v1';
+const ASSETS = ['index.html', 'manifest.json', 'icon-192.png'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // Using map ensures that if one asset fails, it doesn't crash the whole install
-      return Promise.all(
-        ASSETS.map(asset => {
-          return cache.add(asset).catch(err => console.log(`Failed to cache: ${asset}`, err));
-        })
-      );
-    })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
